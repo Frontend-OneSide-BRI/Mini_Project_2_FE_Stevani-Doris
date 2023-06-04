bc9c939989763697c7ca1555ae9fa83749687547
Stevani Doris <stevani.doris@gmail.com>
2023-06-04 19:42:02 +0700

    Add Masonry
    Add Masonry, Modal, Data, Route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from '../components/Carousel';
+import MasonrySection from '../components/MasonrySection';
 
 function Home() {
     return (
         <div className="Home mt-5">
             <Carousel/>
+            <MasonrySection/>
         </div>
     );
 }
