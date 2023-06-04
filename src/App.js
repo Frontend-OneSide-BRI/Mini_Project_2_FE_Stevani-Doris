af7750c3ad1b8eb55fbabfd90076d9770f0d87ab
Stevani Doris <stevani.doris@gmail.com>
2023-06-04 19:12:06 +0700

    Detele hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Navbar/>
+      <Hero/>
     </div>
   );
 }
